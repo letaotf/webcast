77bdde3c3975752c624204c0b026414e73cf2a0a
乐陶 <letao@maihaoche.com>
2018-11-05 11:22:42 +0800

    optional工具类

---
+++ src/main/java/com/taofeng/webcast/common/util/OptionalUtils.java
@@ -0,0 +1,13 @@
+package com.taofeng.webcast.common.util;
+
+/**
+ * <p>optional工具类</p >
+ *
+ * @author: 乐陶（letao@maihaoche.com）
+ * @date: 2018/11/2 下午5:10
+ * @since V1.0
+ */
+public class OptionalUtils {
+
+
+}
